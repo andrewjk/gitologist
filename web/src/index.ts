1aaa428b68557f79580b43d237e20a5c1134acc5
Andrew K <andrewjk@users.noreply.github.com>
2026-04-01 16:02:39 +1100

    Chore: make and index file for packing

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,3 +1,13 @@
-export function fn() {
-	return "Hello, tsdown!";
-}
+import { log } from "console";
+
+import { add } from "./add.ts";
+import { commit } from "./commit.ts";
+import { init } from "./init.ts";
+import { merge } from "./merge.ts";
+import { pull } from "./pull.ts";
+import { push } from "./push.ts";
+import { remoteAdd } from "./remote.ts";
+import { restore } from "./restore.ts";
+import { status } from "./status.ts";
+
+export { add, commit, init, log, merge, pull, push, remoteAdd, restore, status };
